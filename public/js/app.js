369c9531bc23e1904e710bb0d2edfb7d805020f5
shahriar <shahriarenayaty1376@gmail.com>
2019-07-25 12:38:00 +0430

    Remove unnecessary consloe.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log('client side javascript file is loaded')
-
-
-
 const weatherFrom = document.querySelector('form')
 const search  = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
